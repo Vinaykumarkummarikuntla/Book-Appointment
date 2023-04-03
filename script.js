39bcf70687a15a88ac54cd1e01d2f248a8cf2432
Vinaykumarkummarikuntla <kvk.9618@gmail.com>
2023-04-03 09:09:00 +0530

    delete

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var form = document.getElementById("addForm");
 var UserList = document.getElementById("userDetails");
 
-
+var userDetails = []; // initialize empty array to store user details
 
 form.addEventListener("submit", addItem);
 UserList.addEventListener("click", removeItem);
@@ -19,28 +19,24 @@ function addItem(e) {
     gmail: gmail,
     phoneNumber: phoneNumber,
   };
-  const UserDetails = JSON.stringify(details);
-  localStorage.setItem("userDetails", UserDetails);
-
+  
+  userDetails = JSON.stringify(details);
+  localStorage.setItem(userName, userDetails);
   var li = document.createElement("li");
   li.setAttribute("data-username", userName); // add unique identifier
   li.appendChild(document.createTextNode(userName));
-  console.log(li)
 
   var li_2 = document.createElement("li");
   li_2.setAttribute("data-username", userName); // add unique identifier
   li_2.appendChild(document.createTextNode(gmail));
-  console.log(li_2)
 
   var li_3 = document.createElement("li");
   li_3.setAttribute("data-username", userName); // add unique identifier
   li_3.appendChild(document.createTextNode(phoneNumber));
-  console.log(li_3)
 
   var deleteButton = document.createElement("button");
   deleteButton.className = "btn btn-danger btn-sm float-right delete";
   deleteButton.appendChild(document.createTextNode("Delete"));
-
 
   var editButton = document.createElement("button");
   editButton.className = "btn btn-secondary btn-sm float-right Edit";
@@ -51,27 +47,25 @@ function addItem(e) {
   UserList.appendChild(li);
   UserList.appendChild(li_2);
   UserList.appendChild(li_3);
-  
 }
 
 function removeItem(e) {
   if (e.target.classList.contains("delete")) {
     if (confirm("Are You Sure?")) {
       var li = e.target.parentElement;
-      console.log(li)
+
       var userName = li.getAttribute("data-username"); // get unique identifier
-      console.log(userName)
-      var liArray = document.querySelectorAll(`li[data-username="${userName}"]`); // get all li elements with matching identifier
-      console.log(liArray)
-      liArray.forEach(item => {
+
+      var liArray = document.querySelectorAll(
+        `li[data-username="${userName}"]`
+      ); // get all li elements with matching identifier
+
+      liArray.forEach((item) => {
         UserList.removeChild(item); // remove all matching li elements
       });
+      var key = localStorage.key(userName);
+      localStorage.removeItem(key);
+      console.log("deleted");
     }
   }
-  localStorage.removeItem('userDetails');
 }
-
-
-
-  
-  
